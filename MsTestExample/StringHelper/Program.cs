86a944476778c0734909b32c65e0038855501bcb
Serega VRS <vrsserega@gmail.com>
2018-08-17 14:40:05 +0300

    update programm

--- MsTestExample/StringHelper/Program.cs
+++ MsTestExample/StringHelper/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace StringHelper
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
